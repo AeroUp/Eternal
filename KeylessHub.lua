37c78a5fda9118ab129f8c61dcf3f863d3cd9aaf
Aero <vivexter@gmail.com>
2026-05-02 14:31:17 -0400

    Update KeylessHub.lua

--- KeylessHub.lua
+++ KeylessHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://eternal-darkness.org/loaders/2f19a389ad5fe777d79ade7428f0f995.lua"))()
+loadstring(game:HttpGet("https://eternal-darkness.org/loaders/b3feaa34cc2b51d5f26ccd77ada22dae.lua"))()
